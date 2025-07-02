2d3245cc58a6650eb09ae75725847d312896951e
goat-bot-v <kakashib28@gmail.com>
2025-07-02 22:48:06 +0500

    Update thread.js

--- scripts/cmds/thread.js
+++ scripts/cmds/thread.js
@@ -11,7 +11,7 @@ module.exports = {
 			vi: "Quản lý các nhóm chat trong hệ thống bot",
 			en: "Manage group chat in bot system"
 		},
-		category: "owner",
+		category: "𝗢𝗪𝗡𝗘𝗥",
 		guide: {
 			vi: "   {pn} [find | -f | search | -s] <tên cần tìm>: tìm kiếm nhóm chat trong dữ liệu bot bằng tên"
 				+ "\n   {pn} [find | -f | search | -s] [-j | joined] <tên cần tìm>: tìm kiếm nhóm chat trong dữ liệu mà bot còn tham gia bằng tên"
