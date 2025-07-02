492b6c7852d0e3ca86f7c296f3fdfefe22ae0528
goat-bot-v <kakashib28@gmail.com>
2025-07-02 22:36:25 +0500

    Update setrankup.js

--- scripts/cmds/setrankup.js
+++ scripts/cmds/setrankup.js
@@ -12,7 +12,7 @@ module.exports = {
 			vi: "Cấu hình rankup",
 			en: "Configure rankup"
 		},
-		category: "owner",
+		category: "𝗢𝗪𝗡𝗘𝗥",
 		guide: {
 			vi: "   {pn} text <message>: Cấu hình tin nhắn khi thành viên thăng hạng trong box chat của bạn"
 				+ "\n   Với các tham số sau:"
